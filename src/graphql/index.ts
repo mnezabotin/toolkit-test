f753ee73ca1cfb6fa67b5cbe92f0e01183469be6
maxim.mochalov <maxim.mochalov@akvelon.com>
2023-05-30 07:02:48 +0300

    new pat

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client'
 import { setContext } from '@apollo/client/link/context'
 
-const personalAccessToken = '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'
+const personalAccessToken = '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'
 const uri = 'https://api.github.com/graphql'
 
 const httpLink = createHttpLink({
