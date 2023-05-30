c52e1a454c131f5506b6d954dacdf2a17148c174
maxim.mochalov <maxim.mochalov@akvelon.com>
2023-05-30 07:34:55 +0300

    used hash router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import { RouterProvider } from 'react-router-dom'
-import router from './router'
-import Header from './components/Header'
+import { HashRouter, Route, Navigate, Routes } from "react-router-dom";
+import SignIn from './pages/SignIn'
+import Repositories from './pages/Repositories'
+import Repository from './pages/Repository'
 
 export default (): JSX.Element => (
   <>
-    <Header />
-    <RouterProvider router={router} />
+    <HashRouter>
+      <Routes>
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/repositories" element={<Repositories />} />
+        <Route path="/repositories/:id" element={<Repository />} />
+        <Route path="*" element={<Navigate to='/sign-in' replace />} />
+      </Routes>
+    </HashRouter>
   </>
 )
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom'
 import { ApolloProvider } from '@apollo/client'
 import client from './graphql'
-import router from './router'
+import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ApolloProvider client={client}>
-      <RouterProvider router={router} />
+      <App />
     </ApolloProvider>
   </React.StrictMode>,
 )
